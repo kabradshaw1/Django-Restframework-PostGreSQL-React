a95b6dd8153ad611a8ab58fde3921f22b80bbffa
kabradshaw1 <itllbok82@gmail.com>
2022-10-07 20:21:43 -0400

    added client side javascript, some seeds for database, and html submit for get request

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO Feature (name, detail)
+VALUES
+  ('Fast', 'We are fast'),
+  ('Cool', 'We are awesome');
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,22 +1,22 @@
-// var getWeather = function(city) {
-//   // format the github api url
 
-//   var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=11988be7e9227615c1560b01dd5dbd6a";
+var getLocation = $('#item-button').submit(function(item) {
 
-//   // make a get request to url
-//   fetch(apiUrl)
-//     .then(function(response) {
-//       // request was successful
-//       if (response.ok) {
-//         response.json().then(function(data) {
+  var apiUrl = "http://127.0.0.1:8000/api/item/" + item;
+
+  // make a get request to url
+  fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
           
-//           displayWeather(data, city);
-//         });
-//       } else {
-//         alert('Error: City Not Found');
-//       }
-//     })
-//     .catch(function(error) {
-//       alert("Unable to get weather.");
-//   });
-// };
+          console.log(data);
+        });
+      } else {
+        alert('Error: Item Not Found');
+      }
+    })
+    .catch(function(error) {
+      alert("Unable to get items.");
+  });
+});
