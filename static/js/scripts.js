ba90a30bbde59130ef3f5e640b5ee9dee1ad6634
kabradshaw1 <itllbok82@gmail.com>
2022-10-08 15:42:01 -0400

    client side fetch returning proper json

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,20 +1,16 @@
+const getLocation = (formData) => {
+  let queryUrl = '/api/item/' + formData;
 
-var getLocation = $('#item-button').submit(event => {
-  event.preventDefault();
+  
+  // Object.entries(formData).forEach(([key, value]) => {
+  //   queryUrl += `${key}=${value}&`;
+  // });
 
-  const itemName = $('[name="itemName"').value
+  console.log();
 
-  // make a get request to url
-  fetch('api/item', {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(itemName)
-  })
-    .then(function(response) {
-      // request was successful
+  fetch(queryUrl)
+    .then(response => {
+
       if (response.ok) {
         response.json().then(function(data) {
           
@@ -27,4 +23,12 @@ var getLocation = $('#item-button').submit(event => {
     .catch(function(error) {
       alert("Unable to get items.");
   });
+}
+
+const itemSubmit = $('#item-button').submit(event => {
+  event.preventDefault();
+  
+  const itemName = $('[name="itemName"]').val()
+  getLocation(itemName)
+ 
 });
