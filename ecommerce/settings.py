720ac87822ca37129aa54ecbec136b545861caa1
diuguide <everett.diuguid@gmail.com>
2022-11-20 09:20:38 -0500

    removed pw, oops

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'beachhouse',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
